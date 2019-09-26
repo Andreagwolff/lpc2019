1e03fd167b7592ea39dc89a03d456c6c1b5fbc10
Andre Wolff <andre.wolff@rede.ulbra.br>
2019-09-26 10:38:30 -0300

    video aula

---
+++ CrudEFCorePeopleVideoAula/Models/DataContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudEFCorePeopleVideoAula.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Person> People { get; set; }
+    }
+}
