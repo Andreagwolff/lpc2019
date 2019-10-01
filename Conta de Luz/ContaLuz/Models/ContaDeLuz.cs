4deb074573273035276bcbfe70a2a2bfccc1c654
Andre Wolff <andre.wolff@rede.ulbra.br>
2019-10-01 11:15:36 -0300

    trabalho

---
+++ Conta de Luz/ContaLuz/Models/ContaDeLuz.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+namespace MvcDemo.Models
+{
+    public class ContaDeLuz
+    {
+        public ContaDeLuz(){}
+        public int Id { get; set; }
+        [Required]
+        public string DataDeLeitura { get; set; }
+        [Required]
+        public int NumLeitura { get; set; }
+        [Required]
+        public int KwGasto { get; set; }
+        [Required]
+        public float ValorPagar { get; set; }
+        [Required]
+        public string DataDoPagamento { get; set; }
+        [Required]
+        public float MediaDeConsumo { get; set; }
+        
+    }
+}
